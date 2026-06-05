3f52f0f65c86a92095d2f1913c5352d0dd19021d
lemonno <49359040+lemonno2333@users.noreply.github.com>
2026-06-05 13:22:37 +0800

    添加png透明层支持
    RT，可以输出带alpha通道的png了

--- main.cpp
+++ main.cpp
@@ -13,28 +13,17 @@
 #include <cpu.h>
 #include "fmt/format.h"
 
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
-//#include "stb_image_write.h"
-
 class Task {
 public:
     int image_id;
     unsigned char *input_image_data;
     unsigned char *output_image_data;
+    unsigned char *alpha_data;
     int input_w;
     int input_h;
     int input_channel;
     int scale;
     int noise;
-};
-
-class ImageInfo
-{
-public:
-    unsigned char* image_data;
-    int channel;
-    int w;
-    int h;
 };
 
 static RealCUGAN *realcugan;
@@ -48,7 +37,6 @@ static ncnn::ConditionVariable finish_condition;
 #endif
 
 void remove_alpha_channel(unsigned char *image_data, int w, int h) {
-    // remove the alpha channel to avoid exceeding memory limits
     for (int i = 0; i < w * h; i++) {
         image_data[i * 3] = image_data[i * 4];
         image_data[i * 3 + 1] = image_data[i * 4 + 1];
@@ -56,12 +44,46 @@ void remove_alpha_channel(unsigned char *image_data, int w, int h) {
     }
 }
 
-void copy_with_alpha_channel(unsigned char *dst, const unsigned char *src, int w, int h) {
+void extract_alpha_channel(const unsigned char *rgba_data, unsigned char *alpha_out, int w, int h) {
+    for (int i = 0; i < w * h; i++) {
+        alpha_out[i] = rgba_data[i * 4 + 3];
+    }
+}
+
+void upscale_alpha_bilinear(const unsigned char *alpha_in, unsigned char *alpha_out,
+                            int in_w, int in_h, int scale) {
+    int out_w = in_w * scale;
+    int out_h = in_h * scale;
+    for (int y = 0; y < out_h; y++) {
+        float src_y = (float)y / scale;
+        int y0 = (int)src_y;
+        int y1 = (y0 + 1 < in_h) ? y0 + 1 : y0;
+        float fy = src_y - y0;
+        for (int x = 0; x < out_w; x++) {
+            float src_x = (float)x / scale;
+            int x0 = (int)src_x;
+            int x1 = (x0 + 1 < in_w) ? x0 + 1 : x0;
+            float fx = src_x - x0;
+
+            float v00 = alpha_in[y0 * in_w + x0];
+            float v10 = alpha_in[y0 * in_w + x1];
+            float v01 = alpha_in[y1 * in_w + x0];
+            float v11 = alpha_in[y1 * in_w + x1];
+
+            float v = v00 * (1 - fx) * (1 - fy) + v10 * fx * (1 - fy) +
+                      v01 * (1 - fx) * fy + v11 * fx * fy;
+            alpha_out[y * out_w + x] = (unsigned char)(v + 0.5f);
+        }
+    }
+}
+
+void copy_with_alpha_channel(unsigned char *dst, const unsigned char *src,
+                             const unsigned char *alpha, int w, int h) {
     for (int i = 0; i < w * h; i++) {
         dst[0] = src[0];
         dst[1] = src[1];
         dst[2] = src[2];
-        dst[3] = 255;
+        dst[3] = alpha ? alpha[i] : 255;
         dst += 4;
         src += 3;
     }
@@ -69,7 +91,6 @@ void copy_with_alpha_channel(unsigned char *dst, const unsigned char *src, int w
 
 void process_image_success_callback(int image_id, long cost)
 {
-    // callback by stdout =_=
     std::string script = fmt::format(R"($CALLBACK$ {{"eventType": "PROC_END", "image_id": {}, "cost": {}}})", image_id, cost);
     std::cout << script << std::endl;
 }
@@ -93,8 +114,18 @@ static void run_task(Task *task) {
     ncnn::Mat outImage = ncnn::Mat(inImage.w * realcugan->scale, inImage.h * realcugan->scale,
                                    (size_t) inImage.elemsize, (int) inImage.elemsize);
     realcugan->process(inImage, outImage);
-    copy_with_alpha_channel(task->output_image_data, (const unsigned char *) outImage.data, outImage.w,
-                            outImage.h);
+
+    unsigned char *upscaled_alpha = nullptr;
+    if (task->alpha_data) {
+        upscaled_alpha = new unsigned char[outImage.w * outImage.h];
+        upscale_alpha_bilinear(task->alpha_data, upscaled_alpha,
+                               task->input_w, task->input_h, task->scale);
+    }
+    copy_with_alpha_channel(task->output_image_data, (const unsigned char *) outImage.data,
+                            upscaled_alpha, outImage.w, outImage.h);
+    delete[] upscaled_alpha;
+    delete[] task->alpha_data;
+    task->alpha_data = nullptr;
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     auto cost = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
@@ -141,12 +172,17 @@ int process_image(int image_id, unsigned char *input_image_data, unsigned char *
 #endif
         return -1;
     }
+
+    int alpha_size = input_w * input_h;
+    unsigned char *alpha_buf = new unsigned char[alpha_size];
+    extract_alpha_channel(input_image_data, alpha_buf, input_w, input_h);
     remove_alpha_channel(input_image_data, input_w, input_h);
 
     Task *tsk = new Task();
     tsk->image_id = image_id;
     tsk->input_image_data = input_image_data;
     tsk->output_image_data = output_image_data;
+    tsk->alpha_data = alpha_buf;
     tsk->input_w = input_w;
     tsk->input_h = input_h;
     tsk->input_channel = 3;
